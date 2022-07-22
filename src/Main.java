e9fba3fc3c39b20c861fb66835d1aa8929bd737e
Andrey Iljasov <a_iljasov@mail.ru>
2022-07-22 22:46:01 +0300

    Change input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager=new SalesManager(new int[]{100, 200, 300, 500, 0, 10});
+        SalesManager salesManager=new SalesManager(new int[]{100, 200, 300, 500, 0});
         System.out.println(salesManager.max());
     }
 }
